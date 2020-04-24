77efb4fd30dd4a68a7a8815ea453b619b1c4144d
Md. Mirajul Islam <0419052071@grad.cse.buet.ac.bd>
2020-04-24 17:45:15 +0600

    After watching LucidProgramming Tutorial 1

---
+++ Tutorials/part_1_streaming_tweets/twitter_credentials.py
@@ -0,0 +1,6 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "1249024589696389120-6QK9eHEKvErpvDwphzFluAN4NW3NJr"
+ACCESS_TOKEN_SECRET = "yp7jURs6nAfPDHHPpqyAtN5022Emo1aWuo6uvOYi9ROkc"
+CONSUMER_KEY = "hMfgsvq02idWDRWxp5aC6HnX0"
+CONSUMER_SECRET = "whIGrH2BpSHujtJJ5ZGjxlkNBMSP0FxTaeEy8E3al9ytGsTWTW"
