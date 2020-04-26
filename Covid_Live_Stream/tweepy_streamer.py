65dc1af4bb3c9d8d619b9c6ef00ed0ee6d7cf26b
Md. Mirajul Islam <0419052071@grad.cse.buet.ac.bd>
2020-04-26 19:57:17 +0600

    introduced contact tacing analytics

--- Covid_Live_Stream/tweepy_streamer.py
+++ Covid_Live_Stream/tweepy_streamer.py
@@ -4,7 +4,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 import twitter_credentials
-
+import json
 
 class TwitterStreamer():
     """
@@ -13,9 +13,9 @@ class TwitterStreamer():
     def __init__(self):
         pass
 
-    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+    def stream_tweets(self, fetched_tweets_filename, fetched_texts_filename, hash_tag_list):
         # This handles Twitter authetification and the connection to Twitter Streaming API
-        listener = StdOutListener(fetched_tweets_filename)
+        listener = StdOutListener(fetched_tweets_filename, fetched_texts_filename)
         auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
         auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
         stream = Stream(auth, listener)
@@ -28,14 +28,22 @@ class StdOutListener(StreamListener):
     """
     This is a basic listener that just prints received tweets to stdout.
     """
-    def __init__(self, fetched_tweets_filename):
+    def __init__(self, fetched_tweets_filename, fetched_texts_filename):
         self.fetched_tweets_filename = fetched_tweets_filename
+        self.fetched_texts_filename = fetched_texts_filename
+
 
     def on_data(self, data):
         try:
-            print(data)
+            #print(data)
+            json_load = json.loads(data)
+            text = {'text': json_load['text']}
+            print(text)
             with open(self.fetched_tweets_filename, 'a') as tf:
                 tf.write(data)
+            with open(self.fetched_texts_filename, 'a') as tf:
+                tf.write(json.dumps(text))
+                tf.write("\n")
             return True
         except BaseException as e:
             print("Error on_data %s" % str(e))
@@ -49,8 +57,13 @@ class StdOutListener(StreamListener):
 if __name__ == '__main__':
 
     # Authenticate using config.py and connect to Twitter Streaming API.
-    hash_tag_list = ["corona", "covid19", "corona virus", "covid", "covid 19", "covid 2019"]
-    fetched_tweets_filename = "tweets.txt"
+    #hash_tag_list = ["corona", "covid19", "corona virus", "covid", "covid 19", "covid 2019"]
+    hash_tag_list = ["contact tracing", "contact tracking", "corona privacy",
+    "corona application", "covid application", "covid19 application",
+    "corona application", "covid application", "covid19 application"]
+
+    fetched_tweets_filename = "live_tweets.txt"
+    fetched_texts_filename = "live_texts.txt"
 
     twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    twitter_streamer.stream_tweets(fetched_tweets_filename, fetched_texts_filename, hash_tag_list)
